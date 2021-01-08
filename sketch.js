fb5e1323418648ef8f06fd1c4fc8cd40611e3a95
aryansingh37 <aryan.singh.lamba@gmail.com>
2021-01-08 13:31:58 -0800

    added air resistance, currently disabled

--- sketch.js
+++ sketch.js
@@ -1,14 +1,3 @@
-function setup() {
-  createCanvas(1200, 800);
-  p = new Projectile()
-}
-
-function draw() {
-  background(94, 218, 255);
-  rect(0,750,1200,50)
-  p.draw()
-}
-
 class Projectile{
   constructor(){
     this.xPos = 20
@@ -19,6 +8,9 @@ class Projectile{
     this.angle = 60
     this.grav = 980
     this.cor = 1
+    this.dragCoef = 0.5
+    this.area = PI * (this.objWidth / 2) ^ 2 //referring to cross-sectional area
+    this.fluidDensity = 3000/1000000
   }
   
   setVel(vel){
@@ -32,8 +24,19 @@ class Projectile{
   setGravity(g){
     this.grav = g*100
   }
+
+  airResistance(xVel, yVel) {
+    let dragForce = this.dragCoef * this.fluidDensity * (abs(this.vel) ^ 2) * this.area / 2 //determine drag force
+    let newXVel = xVel - dragForce * cos(radians(this.angle)) / 60 //apply drag to x velocity
+    let newYVel = yVel
+    if (yVel > 0) { //apply drag to y velocity
+      newYVel -= dragForce * sin(radians(this.angle))/60
+    } else if (yVel < 0) {
+      newYVel += dragForce * sin(radians(this.angle))/60
+    }
+  }
   
-  updateVars(){ //updates the velocities and positions every draw
+  updateVars() { //updates the velocities and positions every draw
     let xVel = this.vel*cos(radians(this.angle)) //x-component of velocity
     let yVel = this.vel*sin(radians(this.angle)) //y-component of velocity
     if(this.yPos>=750&&frameCount>5){ //checks if ball makes contact with ground
@@ -44,7 +47,7 @@ class Projectile{
     this.xPos += xVel/60 //updates positions
     this.yPos -= yVel/60
     this.vel = sqrt(xVel**2+yVel**2) //rejoins updated velocity components into one magnitude and direction variable
-    this.angle = degrees(atan2(yVel,xVel))
+    this.angle = degrees(atan2(yVel, xVel))
   }
   
   draw(){
@@ -54,3 +57,18 @@ class Projectile{
     this.updateVars()
   }
 }
+
+function setup() {
+  createCanvas(1200, 800);
+  p = new Projectile()
+  test = 600
+}
+
+function draw() {
+  background(94, 218, 255);
+  rect(0,750,1200,50)
+  p.draw()
+  line(test,0,test,800)
+}
+
+let test
